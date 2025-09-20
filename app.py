d7beb46d1b81bb67a02cabed52a43cc6487d9fe4
Consigauditor <consigauditor@multis.local>
2025-09-20 13:56:00 -0300

    Ajuste

--- app.py
+++ app.py
@@ -428,4 +428,6 @@ def baixar_estatico(filename):
     return send_from_directory("static", filename, as_attachment=True)
 
 if __name__ == "__main__":
-    app.run(debug=True, port=8890)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
